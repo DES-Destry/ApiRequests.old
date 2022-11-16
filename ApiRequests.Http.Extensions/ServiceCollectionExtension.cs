624d8d200b21876321c08bb9eeeeec6fb5855989
Andrey Kirik <dev.andrey.kirik@gmail.com>
2022-11-16 16:14:33 +0600

    feat: add ApiRequests.Http.Extensions project

---
+++ ApiRequests.Http.Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApiRequests.Http.Configuration;
+using ApiRequests.Http.Controller;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiRequests.Http.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddHttpController<T>(this IServiceCollection services,
+            Func<IHttpControllerBuilder<IConfiguration>, IHttpController<IConfiguration>> setupAction)
+            where T : class, IHttpControllerBuilder<IConfiguration>, new()
+        {
+            var builder = new T();
+            var controller = setupAction.Invoke(builder);
+            
+            return services.AddSingleton(controller);
+        }
+    }   
+}
