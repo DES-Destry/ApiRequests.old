516c29e9c385721610c0b9da1eb94b9a232ea535
Andrey Kirik <dev.andrey.kirik@gmail.com>
2022-11-17 10:01:42 +0600

    feat: add refreshable token controller injection
    Make setup action for controllers injection nullable. On empty setup action just build controller without additional setting up.

--- ApiRequests.Http.Extensions/ServiceCollectionExtension.cs
+++ ApiRequests.Http.Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ApiRequests.Http.Addons;
 using ApiRequests.Http.Configuration;
 using ApiRequests.Http.Controller;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,12 +9,22 @@ namespace ApiRequests.Http.Extensions
     public static class ServiceCollectionExtension
     {
         public static IServiceCollection AddHttpController<T>(this IServiceCollection services,
-            Func<IHttpControllerBuilder<IConfiguration>, IHttpController<IConfiguration>> setupAction)
+            Func<IHttpControllerBuilder<IConfiguration>, IHttpController<IConfiguration>> setupAction = null)
             where T : class, IHttpControllerBuilder<IConfiguration>, new()
         {
             var builder = new T();
-            var controller = setupAction.Invoke(builder);
-            
+            var controller = setupAction != null ? setupAction.Invoke(builder) : builder.Build();
+
+            return services.AddSingleton(controller);
+        }
+
+        public static IServiceCollection AddRefreshableHttpController<T>(this IServiceCollection services,
+            Func<IRefreshableTokenControllerBuilder<IConfiguration>, IRefreshableTokenController<IConfiguration>>
+                setupAction = null) where T : class, IRefreshableTokenControllerBuilder<IConfiguration>, new()
+        {
+            var builder = new T();
+            var controller = setupAction != null ? setupAction.Invoke(builder) : builder.Build();
+
             return services.AddSingleton(controller);
         }
     }   
