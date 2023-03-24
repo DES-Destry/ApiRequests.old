c043b3b6c2addb58664f4ceef606f40d903e4d6c
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-03-24 15:20:50 +0600

    fix(Amqp.Standard): rabbitmq response json parsing

--- ApiRequests.Amqp.Standard/Dto/RabbitResponseDto.cs
+++ ApiRequests.Amqp.Standard/Dto/RabbitResponseDto.cs
@@ -1,8 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace ApiRequests.Amqp.Standard.Dto
 {
     public class RabbitResponseDto<T>
     {
+        [JsonPropertyName("response")]
         public RabbitResponseDataDto<T> Response { get; set; }
+        
+        [JsonPropertyName("isDisposed")]
         public bool IsDisposed { get; set; }
     }
 }
